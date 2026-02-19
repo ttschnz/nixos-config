ad6cde04430f7da44752417c1ae2eb7c6be82989
Tim T. <ttschnz@protonmail.com>
2026-02-19 11:46:23 +0100

    add udev rules for ti msb board

--- modules/ti-nspire.nix
+++ modules/ti-nspire.nix
@@ -9,5 +9,9 @@
   SUBSYSTEM=="usb", ATTR{idVendor}=="0451", ATTR{idProduct}=="e012", ENV{ID_PDA}="1"
   # TI-Nspire CX II
   SUBSYSTEM=="usb", ATTR{idVendor}=="0451", ATTR{idProduct}=="e022", ENV{ID_PDA}="1"
+  # MSP430 LaunchPad Hub (0451:2046)
+  SUBSYSTEM=="usb", ATTRS{idVendor}=="0451", ATTRS{idProduct}=="2046", MODE="0666", GROUP="plugdev"
+  # MSP430 LaunchPad eZ-FET Debugger (0451:f432)  
+  SUBSYSTEM=="usb", ATTRS{idVendor}=="0451", ATTRS{idProduct}=="f432", MODE="0666", GROUP="plugdev"
   '';
 }
